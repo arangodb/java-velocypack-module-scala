b5b80544d43e23b98d300d81425634b0d63938f0
Oleksandr Vayda <oleksandr.vayda@gmail.com>
2020-02-01 01:37:47 +0100

    arango #8 - add support for various immutable map types

--- src/main/scala/com/arangodb/velocypack/module/scala/VPackScalaModule.scala
+++ src/main/scala/com/arangodb/velocypack/module/scala/VPackScalaModule.scala
@@ -5,6 +5,8 @@ import com.arangodb.velocypack.VPackSetupContext
 import com.arangodb.velocypack.module.scala.internal.VPackScalaSerializers
 import com.arangodb.velocypack.module.scala.internal.VPackScalaDeserializers
 
+import scala.collection.immutable.{HashMap, ListMap, TreeMap}
+
 class VPackScalaModule extends VPackModule {
 
   def setup[C <: VPackSetupContext[C]](context: C): Unit = {
@@ -16,6 +18,7 @@ class VPackScalaModule extends VPackModule {
     context.registerDeserializer(classOf[List[_]], VPackScalaDeserializers.LIST)
 
     context.registerDeserializer(classOf[Map[Any, Any]], VPackScalaDeserializers.MAP)
+
     context.registerDeserializer(classOf[Option[Any]], VPackScalaDeserializers.OPTION, true)
     context.registerDeserializer(classOf[BigInt], VPackScalaDeserializers.BIG_INT)
     context.registerDeserializer(classOf[BigDecimal], VPackScalaDeserializers.BIG_DECIMAL)
@@ -31,7 +34,16 @@ class VPackScalaModule extends VPackModule {
       Nil.getClass
     ).foreach(context.registerSerializer(_, VPackScalaSerializers.SEQ))
 
+    Set(
+      classOf[HashMap.HashMap1[_, _]],
+      classOf[HashMap.HashTrieMap[_, _]],
+      classOf[TreeMap[_, _]],
+      ListMap(Unit -> Unit).getClass,
+      classOf[Map[_, _]]
+    ).foreach(context.registerSerializer(_, VPackScalaSerializers.MAP))
+
     context.registerEnclosingSerializer(classOf[Map[Any, Any]], VPackScalaSerializers.MAP)
+
     context.registerSerializer(classOf[BigInt], VPackScalaSerializers.BIG_INT)
     context.registerSerializer(classOf[Option[Any]], VPackScalaSerializers.OPTION)
     context.registerSerializer(classOf[BigDecimal], VPackScalaSerializers.BIG_DECIMAL)
